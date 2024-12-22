0145402e4dee080b56e6a2053315212cf1c499a9
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-12-22 10:11:37 +0700

    move db util for response utility to response file

--- pkg/response.go
+++ pkg/response.go
@@ -2,8 +2,11 @@ package pkg
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/rs/zerolog/log"
 )
 
 func ValidationResp(fields []ValidationErr) *Response {
@@ -33,6 +36,13 @@ func AutoSelectErrResp(err error) *Response {
 	return resp
 }
 
+// InServerErrResp generates a response for internal server errors
+//
+// Parameters:
+// - err: The error to be logged.
+//
+// Returns:
+// - A Response object for internal server errors.
 func InServerErrResp(err error) *Response {
 	refCode := LogError(err)
 	return &Response{
@@ -41,5 +51,92 @@ func InServerErrResp(err error) *Response {
 		Errors:     nil,
 		Code:       "SERVER_ERR",
 		Ref:        refCode,
+	}
+}
+
+// DbErrResp proccess the given error and generates an appropriate response based on the error type
+//
+// Parameters:
+// - err: The PostgreSQL error to process.
+// - model: A pointer to the struct containing the request input data used for error response generation.
+//
+// Returns:
+// - A Response object based on the error type.
+func DbErrResp(err error, model interface{}) *Response {
+	var resp *Response
+
+	switch err := err.(type) {
+	case *pgconn.PgError:
+		resp = parsePgErr(err, model)
+	}
+
+	return resp
+}
+
+// parsePgErr handles PostgreSQL errors and returns an appropriate response.
+//
+// Parameters:
+// - err: The PostgreSQL error to process.
+// - model: A pointer to the struct containing the request input data used for error response generation.
+//
+// Returns:
+// - A Response object based on the error type.
+func parsePgErr(err *pgconn.PgError, model interface{}) *Response {
+	if err.Code == "23505" {
+		return parseUniqueConstraint(err, model)
+	}
+
+	return InServerErrResp(err)
+}
+
+// parseUniqueConstraint parse unique constraint
+//
+// Parameters:
+// - err: The PostgreSQL error to process.
+// - model: A pointer to the struct containing the request input data used for error response generation.
+//
+// Returns:
+// - A Response object based on the error type.
+func parseUniqueConstraint(err *pgconn.PgError, model interface{}) *Response {
+	constraintName := err.ConstraintName
+
+	reflectType := reflect.TypeOf(model)
+	if reflectType.Kind() != reflect.Ptr {
+		log.Panic().Msgf("expected pointer, got %T", model)
+	}
+
+	if reflectType.Elem().Kind() != reflect.Struct {
+		log.Panic().Msgf("expected pointer to struct, got %T", model)
+	}
+	reflectType = reflectType.Elem()
+
+	var fe *ValidationErr
+
+	for i := 0; i < reflectType.NumField(); i++ {
+		field := reflectType.Field(i)
+		if tagConstraint, ok := field.Tag.Lookup("unique_contraint"); ok {
+			if constraintName == tagConstraint {
+				fPath, ok := field.Tag.Lookup("json")
+				if !ok {
+					fPath = field.Name
+				}
+
+				fe = &ValidationErr{
+					Tag:     "unique",
+					Kind:    field.Type.Kind().String(),
+					Path:    fPath,
+					Message: "This field must be unique",
+				}
+
+				break
+			}
+		}
+	}
+
+	return &Response{
+		StatusCode: fiber.ErrBadRequest.Code,
+		Message:    "Constraint violation error",
+		Code:       string(ErrUniqueConstraint),
+		Errors:     fe,
 	}
 }
